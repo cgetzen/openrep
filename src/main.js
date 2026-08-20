ac6f8924b4c29b8a57398cd944db8ddcd2604a26
Charlie Getzen <charliegetzenlc@gmail.com>
2026-08-19 19:57:29 -0500

    Show recommendation dot only after piece selection

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './style.css.js?v=cburnett-v1';
+import './style.css.js?v=hint-target-v2';
 import { caroKann } from './openings/caro-kann.js';
 import { CoachingTrainerApp } from './coaching-trainer.js';
 
--- src/style.css.js
+++ src/style.css.js
@@ -1,4 +1,4 @@
-const assetVersion = 'cburnett-v1';
+const assetVersion = 'hint-target-v2';
 for (const file of ['./style.css', './coach-overrides.css']) {
   const link = document.createElement('link');
   link.rel = 'stylesheet';
