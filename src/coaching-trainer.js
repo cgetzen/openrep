a7f05f8b6f6eed7b833874491a5dfee61ab658ca
Charlie Getzen <charliegetzenlc@gmail.com>
2026-08-20 00:48:18 -0500

    Bust trainer cache for mode simplification

--- src/coaching-trainer.js
+++ src/coaching-trainer.js
@@ -1,4 +1,4 @@
-import { TrainerApp } from './trainer.js';
+import { TrainerApp } from './trainer.js?v=training-modes-v1';
 import { defaultLineProgress, saveProgress } from './progress.js';
 import { explainWrongMove } from './move-explanations.js';
 import { RepertoireMoveIndex, summarizeExactBranchMatches } from './repertoire-moves.js?v=branch-feedback-v2';
